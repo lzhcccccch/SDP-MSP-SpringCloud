329e724e6d4fc8141650f89a9798a5d22db14de3
liuzhichao <liuzhichao01@cnpc.com.cn>
2021-05-07 14:07:49 +0800

    增加 actuator 监控和增加 RestTemplate 负载均衡示例.

--- MSP-Eureka/src/main/java/com/bcp/sdp/msp/eureka/EurekaApplication.java
+++ MSP-Eureka/src/main/java/com/bcp/sdp/msp/eureka/EurekaApplication.java
@@ -2,7 +2,10 @@ package com.bcp.sdp.msp.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @packageName： com.bcp.sdp.msp.eureka
@@ -20,4 +23,23 @@ public class EurekaApplication {
         SpringApplication.run(EurekaApplication.class, args);
         System.out.println("---------->start");
     }
+
+    /**
+     * @description: 使用 HTTP 方式进行服务间通信时,实现客户端的负载均衡
+     * @param: []
+     * @return: org.springframework.web.client.RestTemplate
+     * @author: liuzhichao 2021-05-07 14:04
+     */       
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public void test() {
+        RestTemplate restTemplate = new RestTemplate();
+        int id = 1001;
+        restTemplate.delete("http://TEST/test/{1}", id);
+    }
+
 }
