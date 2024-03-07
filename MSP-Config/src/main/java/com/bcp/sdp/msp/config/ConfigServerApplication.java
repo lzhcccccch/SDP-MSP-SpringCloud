6572b5af6a1b60a37b3b98973e6844eac4aa0b0a
liuzhichao <liuzhichao00@163.com>
2024-03-07 10:57:02 +0800

    update: config 整合 admin 监控, 取消 Eureka 配置

--- MSP-Config/src/main/java/com/bcp/sdp/msp/config/ConfigServerApplication.java
+++ MSP-Config/src/main/java/com/bcp/sdp/msp/config/ConfigServerApplication.java
@@ -5,14 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * @packageName： com.bcp.sdp.msp.config
- * @className: ConfigApplication
- * @description: TODO
- * @version: v1.0
- * @author: liuzhichao
- * @date: 2020-04-22 15:58
- */
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
@@ -22,4 +14,5 @@ public class ConfigServerApplication {
         SpringApplication.run(ConfigServerApplication.class, args);
         System.out.println("---------->start");
     }
+
 }
